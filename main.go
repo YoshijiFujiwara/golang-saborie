8d79594567c5edfe50e3339e93646f9ee7944467
YoshijiFujiwara <yfbiology2@gmail.com>
2019-05-25 18:54:17 +0900

    uri変更　restの形式に沿って、複数形にする

--- main.go
+++ main.go
@@ -29,18 +29,18 @@ func main() {
 	router.HandleFunc("/login", userController.Login()).Methods("POST")
 
 	// sabota
-	router.HandleFunc("/sabota", sabotaController.Index()).Methods("GET")
-	router.HandleFunc("/sabota", userController.TokenVerifyMiddleware(sabotaController.Store())).Methods("POST") // 認証必要
-	router.HandleFunc("/sabota/{sabotaId}", sabotaController.Show()).Methods("GET")
-	router.HandleFunc("/sabota/{sabotaId}", userController.TokenVerifyMiddleware(sabotaController.Update())).Methods("PUT") // 認証必要
-	router.HandleFunc("/sabota/{sabotaId}", userController.TokenVerifyMiddleware(sabotaController.Destroy())).Methods("DELETE") // 認証必要
+	router.HandleFunc("/sabotas", sabotaController.Index()).Methods("GET")
+	router.HandleFunc("/sabotas", userController.TokenVerifyMiddleware(sabotaController.Store())).Methods("POST") // 認証必要
+	router.HandleFunc("/sabotas/{sabotaId}", sabotaController.Show()).Methods("GET")
+	router.HandleFunc("/sabotas/{sabotaId}", userController.TokenVerifyMiddleware(sabotaController.Update())).Methods("PUT") // 認証必要
+	router.HandleFunc("/sabotas/{sabotaId}", userController.TokenVerifyMiddleware(sabotaController.Destroy())).Methods("DELETE") // 認証必要
 
 	// comment
-	router.HandleFunc("/sabota/{sabotaId}/comments", commentController.Index()).Methods("GET")
-	router.HandleFunc("/sabota/{sabotaId}/comments", userController.TokenVerifyMiddleware(commentController.Store())).Methods("POST") // 認証必要
-	router.HandleFunc("/sabota/{sabotaId}/comments/{commentId}", commentController.Show()).Methods("GET")
-	router.HandleFunc("/sabota/{sabotaId}/comments/{commentId}", userController.TokenVerifyMiddleware(commentController.Update())).Methods("PUT") // 認証必要
-	router.HandleFunc("/sabota/{sabotaId}/comments/{commentId}", userController.TokenVerifyMiddleware(commentController.Destroy())).Methods("DELETE") // 認証必要
+	router.HandleFunc("/sabotas/{sabotaId}/comments", commentController.Index()).Methods("GET")
+	router.HandleFunc("/sabotas/{sabotaId}/comments", userController.TokenVerifyMiddleware(commentController.Store())).Methods("POST") // 認証必要
+	router.HandleFunc("/sabotas/{sabotaId}/comments/{commentId}", commentController.Show()).Methods("GET")
+	router.HandleFunc("/sabotas/{sabotaId}/comments/{commentId}", userController.TokenVerifyMiddleware(commentController.Update())).Methods("PUT") // 認証必要
+	router.HandleFunc("/sabotas/{sabotaId}/comments/{commentId}", userController.TokenVerifyMiddleware(commentController.Destroy())).Methods("DELETE") // 認証必要
 
 	log.Println("Listen on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
